ffa3c2628b87eae520b424a2102539a512cbb2f4
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-06-01 18:03:25 +0900

    no message

--- Lab5/receipter.c
+++ Lab5/receipter.c
@@ -1,36 +1,39 @@
+#include <string.h>
 #include <stdio.h>
 #include <time.h>
 #include "receipter.h"
 
-char receipt[RECEIPT_LENGTH];
-char time_string[64] = "";
-char thank_message[128] = "";
+char g_buffer[BUFFER_SIZE];
+char g_time_line[64];
+char g_message[128];
 
-int daily_orders = 0;
 
-char items[10][64];
-unsigned int item_index = 0;
+unsigned int daily_orders = 0;
+char g_items[10][64];
+unsigned int g_item_index = 0;
+
+
+double g_sub_total = 0;
 double g_tip = 0;
-double sub_total = 0;
+
 
 
 int add_item(const char* name, double price)
 {
-	char buffer[60];
-
-
-	if (item_index == 10)
+	if (g_item_index == 10) {
 		return 0;
+	}
 
-	if (strlen(name) > 25 || price > 999.99f || price < 0)
+	if (strlen(name) > 25u || price > 999.99f || price < 0) {
 		return 0;
+	}
 
 
-	sprintf(buffer, "%33s%17.2f\n", name, price);
-	strcpy(&items[item_index][0], buffer);
+	sprintf(g_buffer, "%33s%17.2f\n", name, price);
+	strcpy(&g_items[g_item_index][0], g_buffer);
 
-	item_index++;
-	sub_total += price;
+	g_item_index++;
+	g_sub_total += price;
 
 	return 1;
 }
@@ -43,45 +46,38 @@ void set_tip(double tip)
 	g_tip = tip;
 }
 
-
 void set_message(const char* message)
 {
-	unsigned int i = 0;
-	size_t length = strlen(message);
+	unsigned int i;
+	size_t lenth = strlen(message);
 
-	char buffer[128];
-
-	if (length > 75)
-	{
+	if (lenth > 75) {
 		return;
 	}
 
-	if (length > 50)
-	{
+	if (lenth > 50) {
+
 		for (i = 0; i < 50; i++) {
-			buffer[i] = message[i];
+			g_buffer[i] = message[i];
 		}
-
-		buffer[i++] = '\n';
-
-		for (; i <= length; i++) {
-			buffer[i] = message[i - 1];
+		g_buffer[i++] = '\n';
+		for (; i <= lenth; i++) {
+			g_buffer[i] = message[i - 1];
 		}
 
 	}
-	else
-	{
-		for (i = 0; i < length; i++) {
-			buffer[i] = message[i];
+	else {
+		for (i = 0; i < lenth; i++) {
+			g_buffer[i] = message[i];
 		}
 	}
-	buffer[i++] = '\n';
-	buffer[i] = '\0';
+	g_buffer[i++] = '\n';
+	g_buffer[i] = '\0';
 
-	strcpy(thank_message, buffer);
+	
+	sprintf(g_message, g_buffer);
 	return;
 }
-
 
 int set_time(time_t timestamp)
 {
@@ -90,86 +86,59 @@ int set_time(time_t timestamp)
 	if (timestamp < 0)
 		return 0;
 
-	sprintf(time_string, "%d-%02d-%02d.%02d:%02d:%02d", 1900 + tm.tm_year, 1+tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	sprintf(g_time_line, "%d-%02d-%02d.%02d:%02d:%02d", 1900 + tm.tm_year, 1 + tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 	return 1;
-	
+
 }
-
-int file_check(const char* filename)
-{
-	int result = 0;
-	FILE* fp = fopen(filename, "r");
-
-	if (fp == NULL) {
-		result = 0;
-	}
-	else {
-		result = 1;
-	}
-
-	fclose(fp);
-
-	return result;
-}
-
 
 int print_receipt(const char* filename, time_t timestamp)
 {
 	int i = 0;
 	double tax = 0, total = 0;
 
-	FILE* receipt_file;
+	char receipt[BUFFER_SIZE];
 
-	if (file_check(filename) == 0)
+	FILE* fp;
+	
+	if (g_item_index == 0) {
 		return 0;
-
-	if (item_index == 0)
-		return 0;
-
-	receipt_file = fopen(filename, "w");
+	}
 
 	sprintf(receipt, "Charles'.Seafood\n");
 	strcat(receipt, "--------------------------------------------------\n");
 
 	set_time(time(NULL));
-	strcat(receipt, time_string);
-	sprintf(&receipt[strlen(receipt)], "%26s%05d\n", " ", ++daily_orders);
+	sprintf(&receipt[strlen(receipt)], "%s%26s%05d\n", g_time_line, " ", ++daily_orders);
 	strcat(receipt, "--------------------------------------------------\n");
 
-	for (i = 0; i < item_index; i++) {
-		strcat(receipt, items[i]);
+	for (i = 0; i < g_item_index; i++) {
+		strcat(receipt, g_items[i]);
 	}
-
 	strcat(receipt, "\n");
 
-	tax = sub_total * 0.05f + 0.005f;
-	total = sub_total + g_tip + tax;
+	tax = g_sub_total * 0.05f + 0.005f;
+	total = g_sub_total + g_tip + tax;
 
-	sprintf(&receipt[strlen(receipt)], "%33s%17.2f\n", "Subtotal", sub_total);
-	sprintf(&receipt[strlen(receipt)], "%33s%17.2f\n", "Tip", g_tip);
-	sprintf(&receipt[strlen(receipt)], "%33s%17.2f\n", "Tax", tax);
-	sprintf(&receipt[strlen(receipt)], "%33s%17.2f\n", "Total", total);
+	sprintf(g_buffer, "%33s%17.2f\n%33s%17.2f\n%33s%17.2f\n%33s%17.2f\n\n", "Subtotal", g_sub_total, "Tip", g_tip, "Tax", tax, "Total", total);
+	strcat(receipt, g_buffer);
 
-	strcat(receipt, "\n");
-
-
-	if (strcmp(thank_message, "") != 0) {
-		strcat(receipt, thank_message);
-	}
+	if (strlen(g_message) > 0)
+		strcat(receipt, g_message);
 
 	strcat(receipt, "==================================================\n");
 
-	sprintf(&receipt[strlen(receipt)], "%50s", "Tax#-51234");	
+	sprintf(&receipt[strlen(receipt)], "%50s", "Tax#-51234");
 
-	
-	fwrite(receipt, 1, strlen(receipt), receipt_file);
 
-	fclose(receipt_file);
-	
-	item_index = 0;
-	sub_total = 0;
+	g_item_index = 0;
+	g_message[0] = '\0';
+	g_sub_total = 0;
 	g_tip = 0;
-	strcpy(thank_message, "");
+
+	fp = fopen(filename, "w");
+	fputs(receipt, fp);
+	fclose(fp);
 
 	return 1;
+
 }
--- Lab5/receipter.h
+++ Lab5/receipter.h
@@ -4,7 +4,7 @@
 #define TRUE (1)
 #define FALSE (0)
 
-#define RECEIPT_LENGTH 4096
+#define BUFFER_SIZE 4096
 #define LINE_LENGTH 50
 
 int add_item(const char* name, double price);
@@ -14,8 +14,6 @@ void set_tip(double tip);
 void set_message(const char* message);
 
 int set_time(time_t timestamp);
-
-int file_check(const char* filename);
 
 int print_receipt(const char* filename, time_t timestamp);
 
