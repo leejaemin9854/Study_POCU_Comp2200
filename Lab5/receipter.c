1101b71ba8f294bd66e45941cabec8e6628e9a42
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-06-02 01:10:43 +0900

    no message

--- Lab5/receipter.c
+++ Lab5/receipter.c
@@ -20,128 +20,130 @@ double g_tip = 0;
 
 int add_item(const char* name, double price)
 {
-	if (g_item_index == 10) {
-		return 0;
-	}
+    if (g_item_index == 10) {
+        return 0;
+    }
 
-	if (strlen(name) == 0 || strlen(name) > 25u || price > 999.99f || price < 0) {
-		return 0;
-	}
+    if (strlen(name) == 0 || strlen(name) > 25u || price > 999.99f || price < 0) {
+        return 0;
+    }
 
 
-	sprintf(g_buffer, "%33s%17.2f\n", name, price);
-	strcpy(&g_items[g_item_index][0], g_buffer);
+    sprintf(g_buffer, "%33s%17.2f\n", name, price);
+    strcpy(&g_items[g_item_index][0], g_buffer);
 
-	g_item_index++;
-	g_sub_total += price;
+    g_item_index++;
+    g_sub_total += price;
 
-	return 1;
+    return 1;
 }
 
 void set_tip(double tip)
 {
-	if (tip < 0 || tip > 999.99f)
-		return;
+    if (tip < 0 || tip > 999.99f)
+        return;
 
-	g_tip = tip;
+    g_tip = tip;
 }
 
 void set_message(const char* message)
 {
-	unsigned int i;
-	size_t lenth = strlen(message);
+    unsigned int i;
+    size_t lenth = strlen(message);
 
-	if (lenth > 75) {
-		return;
-	}
+    if (lenth > 75) {
+        return;
+    }
 
-	if (lenth > 50) {
+    if (lenth > 50) {
 
-		for (i = 0; i < 50; i++) {
-			g_message[i] = message[i];
-		}
-		g_message[i++] = '\n';
-		for (; i <= lenth; i++) {
-			g_message[i] = message[i - 1];
-		}
+        for (i = 0; i < 50; i++) {
+            g_message[i] = message[i];
+        }
+        g_message[i++] = '\n';
+        for (; i <= lenth; i++) {
+            g_message[i] = message[i - 1];
+        }
 
-	}
-	else {
-		for (i = 0; i < lenth; i++) {
-			g_message[i] = message[i];
-		}
-	}
-	g_message[i++] = '\n';
-	g_message[i] = '\0';
+    } else {
+        for (i = 0; i < lenth; i++) {
+            g_message[i] = message[i];
+        }
+    }
+    g_message[i++] = '\n';
+    g_message[i] = '\0';
 
-	return;
+    return;
 }
 
 int set_time(time_t timestamp)
 {
-	struct tm* t = gmtime(&timestamp);
+    struct tm* t = gmtime(&timestamp);
 
-	if (t == NULL)
-		return 0;
+    if (t == NULL) {
+        return 0;
+    }
 
-	sprintf(g_time_line, "%d-%02d-%02d.%02d:%02d:%02d", 1900 + t->tm_year, 1 + t->tm_mon, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
-	return 1;
+    sprintf(g_time_line, "%d-%02d-%02d.%02d:%02d:%02d", 1900 + t->tm_year, 1 + t->tm_mon, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+    return 1;
 }
 
 int print_receipt(const char* filename, time_t timestamp)
 {
-	int i = 0;
-	double tax = 0, total = 0;
+    int i = 0;
+    double tax = 0, total = 0;
 
-	char receipt[BUFFER_SIZE];
+    char receipt[BUFFER_SIZE];
 
-	FILE* fp;
+    FILE* fp;
 
-	if (g_item_index == 0) {
-		return 0;
-	}
+    if (g_item_index == 0) {
+        return 0;
+    }
 
-	sprintf(receipt, "Charles'.Seafood\n");
-	strcat(receipt, "--------------------------------------------------\n");
+    sprintf(receipt, "Charles'.Seafood\n");
+    strcat(receipt, "--------------------------------------------------\n");
 
-	set_time(timestamp);
+    set_time(timestamp);
 
-	sprintf(&receipt[strlen(receipt)], "%s%26s%05d\n", g_time_line, " ", ++g_daily_orders);
-	strcat(receipt, "--------------------------------------------------\n");
+    sprintf(&receipt[strlen(receipt)], "%s%26s%05d\n", g_time_line, " ", ++g_daily_orders);
+    strcat(receipt, "--------------------------------------------------\n");
 
-	for (i = 0; i < g_item_index; i++) {
-		strcat(receipt, g_items[i]);
-	}
-	strcat(receipt, "\n");
+    for (i = 0; i < g_item_index; i++) {
+        strcat(receipt, g_items[i]);
+    }
+    strcat(receipt, "\n");
 
-	tax = g_sub_total * 0.05f + 0.005f;
-	total = g_sub_total + g_tip + tax;
+    tax = g_sub_total * 0.05f + 0.005f;
+    total = g_sub_total + g_tip + tax;
 
-	sprintf(g_buffer, "%33s%17.2f\n", "Subtotal", g_sub_total);
-	if (g_tip > 0)
-		sprintf(&g_buffer[strlen(g_buffer)], "%33s%17.2f\n", "Tip", g_tip);
+    sprintf(g_buffer, "%33s%17.2f\n", "Subtotal", g_sub_total);
+    if (g_tip > 0) {
+        sprintf(&g_buffer[strlen(g_buffer)], "%33s%17.2f\n", "Tip", g_tip);
+    }
 
-	sprintf(&g_buffer[strlen(g_buffer)], "%33s%17.2f\n", "Tax", tax);
-	sprintf(&g_buffer[strlen(g_buffer)], "%33s%17.2f\n\n", "Total", total);
-	strcat(receipt, g_buffer);
+    sprintf(&g_buffer[strlen(g_buffer)], "%33s%17.2f\n", "Tax", tax);
+    sprintf(&g_buffer[strlen(g_buffer)], "%33s%17.2f\n\n", "Total", total);
+    strcat(receipt, g_buffer);
 
-	if (strlen(g_message) > 0)
-		strcat(receipt, g_message);
+    if (strlen(g_message) > 0) {
+        strcat(receipt, g_message);
+    }
 
-	strcat(receipt, "==================================================\n");
+    strcat(receipt, "==================================================\n");
 
-	sprintf(&receipt[strlen(receipt)], "%50s", "Tax#-51234");
+    sprintf(&receipt[strlen(receipt)], "%50s", "Tax#-51234");
 
 
-	g_item_index = 0;
-	g_message[0] = '\0';
-	g_sub_total = 0;
-	g_tip = 0;
+    g_item_index = 0;
+    g_message[0] = '\0';
+    g_sub_total = 0;
+    g_tip = 0;
 
-	fp = fopen(filename, "w");
-	fputs(receipt, fp);
-	fclose(fp);
+    fp = fopen(filename, "w");
+    fputs(receipt, fp);
+    fclose(fp);
 
-	return 1;
+    return 1;
 
 }
