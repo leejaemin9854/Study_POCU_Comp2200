780539b0b052566fdfd210e121e07a6759162744
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-06-02 16:51:54 +0900

    no message

--- Lab5/receipter.c
+++ Lab5/receipter.c
@@ -20,20 +20,16 @@ double g_tip = 0;
 
 int add_item(const char* name, double price)
 {
-    char food_name[26];
-
     if (g_item_index == 10) {
         return 0;
     }
 
-    if (strlen(name) <= 25u) {
-        sprintf(g_buffer, "%33s%17.2f\n", name, price);
-    } else {
-        strncpy(food_name, name, 25);
-        food_name[25] = '\0';
-        sprintf(g_buffer, "%33s%17.2f\n", food_name, price);
+    if (strlen(name) == 0 || strlen(name) > 25u || price > 999.99f || price < 0) {
+        return 0;
     }
 
+
+    sprintf(g_buffer, "%33s%17.2f\n", name, price);
     strcpy(&g_items[g_item_index][0], g_buffer);
 
     g_item_index++;
