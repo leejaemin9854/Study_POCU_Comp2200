c3fef5cd92269f03dfc5357577b8b833e3a6e502
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-06-02 15:46:15 +0900

    no message

--- Lab5/receipter.c
+++ Lab5/receipter.c
@@ -20,12 +20,11 @@ double g_tip = 0;
 
 int add_item(const char* name, double price)
 {
-    int test, test1;
     if (g_item_index == 10) {
         return 0;
     }
 
-    if (strlen(name) == 0 || price > 999.99f || price < 0) {
+    if (strlen(name) == 0 || strlen(name) > 25u || price > 999.99f || price < 0) {
         return 0;
     }
 
@@ -146,6 +145,7 @@ int print_receipt(const char* filename, time_t timestamp)
 
     fp = fopen(filename, "w");
     fputs(receipt, fp);
+    printf("%s\n", receipt);
     fclose(fp);
 
     return 1;
