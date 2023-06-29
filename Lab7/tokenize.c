756b2e48df8b05a166a5f021f241c3ee4707fb48
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-06-29 14:51:21 +0900

    no message

--- Lab7/tokenize.c
+++ Lab7/tokenize.c
@@ -5,77 +5,77 @@
 
 int inchar(const char ch, const char* str)
 {
-	while (*str != 0) {
-		if (ch == *str) {
-			return 1;
-		}
-		str++;
-	}
+    while (*str != 0) {
+        if (ch == *str) {
+            return 1;
+        }
+        str++;
+    }
 	
-	return 0;
+    return 0;
 }
 
 char** tokenize_malloc(const char* str, const char* delim)
 {
-	char** result = NULL;
-	size_t token_len = 0;
+    char** result = NULL;
+    size_t token_len = 0;
 
-	char* token;
-	size_t index;
+    char* token;
+    size_t index;
 	
-	const char* ptr = str;
+    const char* ptr = str;
 	
-	while (*ptr != 0) {
-		if (!inchar(*ptr, delim)) {
-			ptr++;
-		}
-		else {
-			while (inchar(*ptr, delim)) {
-				if (*ptr == 0)
-					break;
-				ptr++;
-			}
-			token_len++;
-		}
-	}
-	token_len++;
+    while (*ptr != 0) {
+        if (!inchar(*ptr, delim)) {
+            ptr++;
+        } else {
+            while (inchar(*ptr, delim)) {
+                if (*ptr == 0) {
+                    break;
+                }
+                ptr++;
+            }
+            token_len++;
+        }
+    }
+    token_len++;
 	
-	result = (char**)malloc((token_len + 1) * sizeof(char*));
+    result = (char**)malloc((token_len + 1) * sizeof(char*));
 
-	ptr = str;
-	index = 0;
-	while (*ptr != 0) {
-		if (!inchar(*ptr, delim)) {
-			ptr++;
-		}
-		else {
-			if (ptr != str) {
-				token = (char*)calloc(ptr - str + 1, 1);
-				strncat(token, str, ptr - str);
+    ptr = str;
+    index = 0;
+    while (*ptr != 0) {
+        if (!inchar(*ptr, delim)) {
+            ptr++;
+        } else {
+            if (ptr != str) {
+                token = (char*)calloc(ptr - str + 1, 1);
+                strncat(token, str, ptr - str);
 
-				result[index++] = token;
+                result[index++] = token;
 
-				ptr++;
-			}
-			else {
-				while (inchar(*ptr, delim)) {
-					if (*ptr == 0)
-						break;
-					ptr++;
-				}
-			}
+                ptr++;
+            }
+            else {
+                while (inchar(*ptr, delim)) {
+                    if (*ptr == 0) {
+                        break;
+                    }
+                    ptr++;
+                }
+            }
 
-			str = ptr;
-		}
-	}
-	if (ptr != str) {
-		token = (char*)calloc(ptr - str + 1, 1);
-		strncat(token, str, ptr - str);
+            str = ptr;
+        }
+    }
+    if (ptr != str) {
+        token = (char*)calloc(ptr - str + 1, 1);
+        strncat(token, str, ptr - str);
 
-		result[index++] = token;
-	}
-	result[index] = NULL;
+        result[index++] = token;
+    }
+    result[index] = NULL;
 
-	return result;
+    return result;
 }
 
