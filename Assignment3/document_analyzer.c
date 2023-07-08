68b08fc7dc273e91bb6052e64b27792d27e59872
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-07-09 02:58:07 +0900

    no message

--- Assignment3/document_analyzer.c
+++ Assignment3/document_analyzer.c
@@ -1,174 +1,235 @@
 #include <stdio.h>
-#include "document_analyzer.h"
 #include <stdlib.h>
 #include <string.h>
+#include "document_analyzer.h"
 
-#define delims_word " ,"
-#define delims_sentence ".!?"
-#define delims_paragraph "\n\r"
 
+char* delims_word = " ,";
+char* delims_sentence = ".!?";
+char* delims_paragraph = "\n\r";
+
+char* g_document_str;
 
 char**** g_document;
 
+
+int isAlpha(const char ch)
+{
+	return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z');
+}
+
+int isNum(const char ch)
+{
+	return ch >= '0' && ch <= '9';
+}
+
 int char_in_str(const char ch, const char* str)
 {
-    while (*str != 0) {
-        if (ch == *str) {
-            return 1;
-        }
-        str++;
-    }
+	while (*str != '\0') {
+		if (ch == *str) {
+			return 1;
+		}
+		*str++;
+	}
+	return 0;
+}
 
-    return 0;
+int set_document_string(const char* file_name)
+{
+	FILE* fp;
+	char ch;
+	unsigned int index;
+
+	if ((fp = fopen(file_name, "rb")) == NULL) {
+		return 0;
+	}
+
+	fseek(fp, 0, SEEK_END);
+	g_document_str = malloc((ftell(fp) + 1) * sizeof(char));
+
+	fseek(fp, 0, SEEK_SET);
+	index = 0;
+	while ((ch = fgetc(fp)) != EOF) {
+		g_document_str[index++] = ch;
+	}
+	g_document_str[index] = '\0';
+
+
+	return 1;
+}
+
+void set_document_memory(void)
+{
+	char* ptr;
+
+	unsigned int cnt;
+	unsigned int i, j;
+
+
+	/*paragraph size set*/
+	ptr = g_document_str;
+	cnt = 0;
+	while (*ptr != '\0') {
+
+		if (char_in_str(*ptr, delims_paragraph)) {
+
+			cnt++;
+			while (char_in_str(*ptr, delims_paragraph) && *ptr != '\0') {
+				ptr++;
+			}
+
+		}
+		else {
+			ptr++;
+		}
+
+	}
+	cnt++;
+	g_document = malloc(cnt * sizeof(char***));
+
+
+	/*sentence size set*/
+	ptr = g_document_str;
+	for (i = 0; i < _msize(g_document) / sizeof(char***); i++) {
+
+		cnt = 0;
+		while (*ptr != '\0') {
+
+			if (char_in_str(*ptr, delims_paragraph)) {
+
+				while (char_in_str(*ptr, delims_paragraph) && *ptr != '\0') {
+					ptr++;
+				}
+				break;
+			}
+			else {
+				if (char_in_str(*ptr, delims_sentence)) {
+					cnt++;
+				}
+				ptr++;
+			}
+
+		}
+		g_document[i] = malloc(cnt * sizeof(char**));
+
+	}
+
+
+	/*word size set*/
+	ptr = g_document_str;
+	for (i = 0; i < _msize(g_document) / sizeof(char***); i++) {
+
+		for (j = 0; j < _msize(g_document[i]) / sizeof(char**); j++) {
+
+			cnt = 0;
+			while (*ptr != '\0') {
+
+				if (char_in_str(*ptr, delims_sentence)) {
+
+					while (!(isAlpha(*ptr) || isNum(*ptr)) && *ptr != '\0') {
+						ptr++;
+					}
+
+					break;
+				}
+				else {
+
+					if (char_in_str(*ptr, delims_word)) {
+
+						cnt++;
+
+						while (char_in_str(*ptr, delims_word)) {
+							ptr++;
+						}
+					}
+					else {
+						ptr++;
+					}
+
+				}
+
+
+			}
+			g_document[i][j] = malloc((cnt + 1) * sizeof(char*));
+
+		}
+
+
+	}
+
+
+	for (i = 0; i < _msize(g_document) / sizeof(char***); i++) {
+
+		for (j = 0; j < _msize(g_document[i]) / sizeof(char**); j++) {
+			printf("[%d]-%d-%d\n", i, j, _msize(g_document[i][j]) / sizeof(char*));
+		}
+
+	}
+
+	return;
+}
+
+void set_document_word(void)
+{
+	return;
 }
 
 int load_document(const char* document)
 {
-    char**** result;
+	if (set_document_string(document) == 0) {
+		return 0;
+	}
 
-    char* document_str;
-    char ch;
-    unsigned int index = 0;
-
-
-    char* ptr;
-    char* token;
-    int cnt;
-    size_t size;
-
-    unsigned int i, j;
-
-    FILE* fp = fopen(document, "rb");
-    if (fp == NULL) {
-        return FALSE;
-    }
-    fseek(fp, 0, SEEK_END);
-    document_str = (char*)calloc(sizeof(char), ftell(fp));
-    fseek(fp, 0, SEEK_SET);
-    while ((ch = fgetc(fp)) != EOF) {
-        document_str[index++] = ch;
-    }
-
-    ptr = document_str;
-    cnt = 0;
-    while (*ptr != '\0') {
-
-        if (char_in_str(*ptr, delims_paragraph)) {
-
-            while (char_in_str(*ptr, delims_paragraph)) {
-                if (*ptr == '\0') {
-                    break;
-                }
-                ptr++;
-            }
-            cnt++;
-
-        }
-        else {
-
-            ptr++;
-
-        }
-
-    }
-    cnt++;
-    result = (char****)calloc(sizeof(char***), cnt);
-
-    ptr = document_str;
-    size = _msize(result) / sizeof(char***);
-    for (i = 0; i < size; i++) {
-        
-        cnt = 0;
-        while (*ptr != '\0') {
-
-            if (char_in_str(*ptr, delims_paragraph)) {
-
-                while (char_in_str(*ptr, delims_paragraph) && *ptr != '\0') {
-                    ptr++;
-                }
-
-                break;
-            }
-            else {
-
-                if (char_in_str(*ptr, delims_sentence)) {
-
-                    while (char_in_str(*ptr, delims_sentence) && *ptr != '\0') {
-                        ptr++;
-                    }
-
-                    cnt++;
-                }
-                else {
-                    ptr++;
-                }
-
-            }
-
-        }
-        result[i] = (char***)calloc(sizeof(char**), cnt);
-
-    }
-
-
-    
-    
-
-
-    free(document_str);
-    document_str = NULL;
-
-
-    return TRUE;
+	return 1;
 }
 
 void dispose(void)
 {
-    return;
+	return;
 }
 
 unsigned int get_total_word_count(void)
 {
-    return 0;
+	return 0;
 }
 
 unsigned int get_total_sentence_count(void)
 {
-    return 0;
+	return 0;
 }
 
 unsigned int get_total_paragraph_count(void)
 {
-    return 0;
+	return 0;
 }
 
 const char*** get_paragraph_or_null(const unsigned int paragraph_index)
 {
-    return NULL;
+	return NULL;
 }
 
 unsigned int get_paragraph_word_count(const char*** paragraph)
 {
-    return 0;
+	return 0;
 }
 
 unsigned int get_paragraph_sentence_count(const char*** paragraph)
 {
-    return 0;
+	return 0;
 }
 
 const char** get_sentence_or_null(const unsigned int paragraph_index, const unsigned int sentence_index)
 {
-    return NULL;
+	return NULL;
 }
 
 unsigned int get_sentence_word_count(const char** sentence)
 {
-    return 0;
+	return 0;
 }
 
 int print_as_tree(const char* filename)
 {
-    return 0;
+	return 0;
 }
 
