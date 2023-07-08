9821e3a618e80277589e9241276a36e2c03a3014
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-07-08 17:40:36 +0900

    no message

--- Assignment3/document_analyzer.c
+++ Assignment3/document_analyzer.c
@@ -87,8 +87,10 @@ void set_document_memory(void)
 	cnt++;
 	g_document = calloc(sizeof(char***), cnt);
 
-	g_document[0] = malloc(1 * sizeof(char**));
-	g_document[1] = malloc(1 * sizeof(char**));
+
+	for (i = 0; i < _msize(g_document) / sizeof(char***); i++) {
+		g_document[i] = malloc(1 * sizeof(char**));
+	}
 
 
 
