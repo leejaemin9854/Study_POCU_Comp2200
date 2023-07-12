7c9d05067632f5dd2f63e15da052119c7c78cc1f
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-07-12 18:37:06 +0900

    no message

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -11,17 +11,18 @@
 
 #define RANGE_DESC(curr, max, min) for((curr) = (max); (curr) >= (min); (curr)--)
 
-void set_func(int* ary, int start, int count, int value);
+int set_func(int* ary, int start, int count, int value);
 
-#define SET(ary, start, count, value) set_func((int*)(ary), (int)(start), (int)(count), (int)(value));
+#define SET(ary, start, count, value) (set_func((int*)(ary), (int)(start), (int)(count), (int)(value)));
 
-void set_func(int* ary, int start, int count, int value)
+int set_func(int* ary, int start, int count, int value)
 {
 	int i = start;
 	for (; i < start + count; i++) {
 		ary[i] = value;
 	}
 
+	return 1;
 }
 
 
