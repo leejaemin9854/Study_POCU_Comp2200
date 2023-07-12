8291606657c0a65b5f0187345a5f0300d3fbe2f0
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-07-12 14:36:58 +0900

    no message

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -1,31 +1,16 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define MAX(x, y) (x >= y ? x : y)
+#define MAX(x, y) (0)
 
-#define MIN(x, y) (x <= y ? x : y)
+#define MIN(x, y) (0)
 
-#define ABS(x) (x >= 0 ? x : x * -1)
+#define ABS(x) (0)
 
+#define RANGE(curr, min, max)
 
-#define RANGE(curr, min, max) for(curr = min; curr <= max; curr++)
+#define RANGE_DESC(curr, max, min)
 
-#define RANGE_DESC(curr, max, min) for(curr = max; curr >= min; curr--)
-
-#define SET(ary, start, count, value) set_func((int*)ary, (int)start, (int)count, (int)value);
-
-
-
-void set_func(int* ary, int start, int count, int value);
-
-
-void set_func(int* ary, int start, int count, int value)
-{
-	int i = start;
-	for (; i < start + count; i++) {
-		ary[i] = value;
-	}
-
-}
+#define SET(ary, start, count, value)
 
 #endif /* MACROS_H */
