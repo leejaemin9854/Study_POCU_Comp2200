37a5f618f45a7f6e08fbe3088c88661ffc96cf4d
DESKTOP-Q5L4SNM\Adminstrator <leejaemin9854@daum.net>
2023-07-17 02:17:10 +0900

    no message

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -5,7 +5,7 @@
 
 #define MIN(x, y) ((x) <= (y) ? (x) : (y))
 
-#define ABS(x) (0)
+#define ABS(x) ((double)(x) >= 0 ? (x) : (x) * -1)
 
 #define RANGE(curr, min, max) for((curr) = (min); (curr) <= (max); (curr)++)
 
